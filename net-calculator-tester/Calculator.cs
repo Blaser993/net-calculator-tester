0a76de131eae506c59c4d00ccec56a7db9bc4f59
Blaser993 <corradodepinto.design@gmail.com>
2023-09-25 15:00:15 +0200

    Aggiungere i file di progetto.

---
+++ net-calculator-tester/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    public class Calculator
+    {
+        float Add (float x, float y) 
+        {
+            return x + y;
+        }
+
+        float Subtract(float x, float y) 
+        {
+            return x - y;
+        }
+
+        float Multiply(float x, float y) 
+        {
+            return x * y;
+        }
+
+        float Divide(float x, float y) 
+        {
+            return x / y;
+        }
+    }
+}
